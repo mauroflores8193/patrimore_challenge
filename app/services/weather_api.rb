f0f0474c124ea8a16e1498642967e655a781ac6c
MauroFlores <mauroflores8193@gmail.com>
2023-03-15 00:48:01 -0500

    se agrega key weather_api.rb en credentials

--- app/services/weather_api.rb
+++ app/services/weather_api.rb
@@ -4,7 +4,7 @@ require 'uri'
 class WeatherApi
   def current(iata)
     Rails.cache.fetch(['weather_api', iata], expires_in: 1.minutes) do
-      uri = URI("http://api.weatherapi.com/v1/current.json?" + { 'key': '3ba8a7019941413685353040231403', 'lang': 'es', 'q': "iata:#{iata}" }.to_query)
+      uri = URI("http://api.weatherapi.com/v1/current.json?" + { key: Rails.application.credentials.dig(:wheater_api, :key), lang: 'es', q: "iata:#{iata}" }.to_query)
       response = Net::HTTP.get_response(uri)
       response.is_a?(Net::HTTPSuccess) ? JSON.parse(Net::HTTP.get(uri)) : nil
     end
